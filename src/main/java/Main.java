1706fdaf4138ad958acf1735c3497f573878c0d5
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-09-05 16:15:00 +0200

    basic text extraction from pdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ddddddddddddd");
+
+        try(PDDocument document = PDDocument.load(new File("C:\\Users\\mateu\\Desktop\\Nowy folder\\pdf to excel\\pdf\\test.pdf"))) {
+            PDFTextStripper pdfTextStripper = new PDFTextStripper();
+            pdfTextStripper.setEndPage(1);
+            String text = pdfTextStripper.getText(document);
+            System.out.println(text);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
     }
 }
